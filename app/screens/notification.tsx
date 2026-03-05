b1989aedfd87f055c01eee6fd1e51408af549627
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-03-05 18:34:31 +0530

    changed the API url to render

--- app/screens/notification.tsx
+++ app/screens/notification.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Notification() {
   return (
     <View style={{flex:1, flexDirection:"row", alignItems:"center", justifyContent:"center"}}>
-      <Text>Notification Screens</Text>
+      <Text>Notification Screen</Text>
     </View>
   );
 }
