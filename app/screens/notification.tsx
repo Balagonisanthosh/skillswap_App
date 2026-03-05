be230464fbf78416ec6c0860cc842316d9f3d312
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-03-05 18:40:49 +0530

    Initial commit

--- app/screens/notification.tsx
+++ app/screens/notification.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Notification() {
   return (
     <View style={{flex:1, flexDirection:"row", alignItems:"center", justifyContent:"center"}}>
-      <Text>Notification Screen</Text>
+      <Text>Notification Screens</Text>
     </View>
   );
 }
